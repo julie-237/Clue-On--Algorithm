00ee72678775910600d26069fadb4d54faab38c8
unknown <blandinedjoumsie@gmail.com>
2024-08-05 19:09:28 +0200

    In comments, added steps to follow to build up my algorithm

---
+++ main.py
@@ -0,0 +1,19 @@
+"""Step 1:
+Ask for the number of sleeping hours, store it
+Ask for the number of working hours, store it 
+Ask for the number of relaxing hours, store it 
+Compute available hours = 24 - sleeping hours - working hours - relaxing hours - 3
+print available hours
+
+
+Step 2:
+Ask for the number of sleeping hours, store it
+Ask for the number of working hours, store it 
+Ask for the number of week day relaxing hours, store it 
+Ask for the number of week end relaxing hours, store it 
+Compute week day available hours = 24 - sleeping hours - working hours - week day relaxing hours - 3
+Compute week day available hours = 24 - sleeping hours - week end relaxing hours - 3
+Compute the total available hours per week   = 5* available hours per week day + 2* available hours per week end
+
+
+print available hours (week day, week end, total per week)"""
