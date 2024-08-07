fd5b15b7642b51dac0de2745aeb07cda0df5177a
unknown <blandinedjoumsie@gmail.com>
2024-08-07 20:56:08 +0200

    bought some modifications to my layout and added tips on how to print strings and variables

--- main.py
+++ main.py
@@ -22,4 +22,17 @@ sleeping_hours = int(input("howlong do you sleep? "))
 working_hours = int(input("howlong do you work? "))
 relaxing_hours = int(input("howlong do you relax? "))
 available_study_hours_per_day = 24 - sleeping_hours - working_hours - relaxing_hours - 3
-print("so you have " + available_study_hours_per_day + " hours to study per day")
+print("so you have ", available_study_hours_per_day, " hours to study per day")
+
+print("and you have ", sleeping_hours, " hours to sleep per day")
+print("and you have ", working_hours, " hours to work per day")
+print("and you have ", relaxing_hours, " hours to relax per day")
+
+print(f"and you have {relaxing_hours} hours to relax per day")
+
+message_template = "and you have {} hours to {} per day"
+print(message_template.format(sleeping_hours, "sleep"))
+print(message_template.format(working_hours, "work"))
+print(message_template.format(relaxing_hours, "relax"))
+
+print(sleeping_hours, available_study_hours_per_day)
